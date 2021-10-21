760808be2499093bab5e5eea6a4c5bd9e0062aa1
LOKKEE <kilian_peters@gmx.de>
2021-10-21 18:34:51 +0200

    Added a contact form and footer component and improved a bunch of things

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-/* Mobile responsive width */
-const mobileWidth = 600;
+/* The same breakpoints as the CSS ones, for responsibility */
+const smallBreakpoint = 600;
+const mediumBreakpoint = 800;
 
 /* fullPage.js configuration */
 new fullpage('#app', {
@@ -7,12 +8,13 @@ new fullpage('#app', {
   navigation: true,
   navigationPosition: 'left',
   scrollingSpeed: 650,
-  responsiveWidth: mobileWidth,
+  responsiveWidth: mediumBreakpoint,
+  scrollOverflow: true,
   anchors: ['home', 'about', 'work', 'contact', 'sourceCode'],
   onLeave: function (origin, destination) {
-    if (destination.isFirst && window.innerWidth > mobileWidth) {
+    if (destination.isFirst && window.innerWidth > mediumBreakpoint) {
       document.querySelector('.navigation').style.background = 'none';
-    } else if (origin.isFirst && window.innerWidth > mobileWidth) {
+    } else if (origin.isFirst && window.innerWidth > mediumBreakpoint) {
       document.querySelector('.navigation').style.background = 'rgba(var(--rgb-dark-not-black), var(--opacity-not-much-transparency))';
     }
   }
@@ -41,7 +43,7 @@ function fadeOutLoadingScreen() {
 }
 
 /* Typewriter configuration */
-const texts = ["developer", "graphic designer", "coffeeholic", "freelancer", "globetrotter"];
+const texts = ["Developer", "Problem solver", "Graphic designer", "Coffeeholic", "Freelancer", "Globetrotter"];
 let index = 0;
 
 document.getElementById("typewriter").addEventListener("animationend", updateTypewriter);
@@ -56,9 +58,9 @@ function updateTypewriter() {
     let textLength = texts[index].length;
     let speed = textLength / 2.3;
 
-    text.style.animation = 'typewriter-typing ' + speed + 's steps(' + textLength + ', end), typewriter-blink-caret .75s step-end infinite';
+    text.style.animation = 'typewriter-typing ' + speed + 's steps(' + textLength + ', end)';
     (index === texts.length - 1) ? index = 0 : index++;
-  }, 550);
+  }, 450);
 }
 
 /* Toggle mobile navigation-menu method */
@@ -88,3 +90,45 @@ function openEmail() {
 document.querySelector('#openEmail').addEventListener("click", function () {
   openEmail()
 });
+
+/* Form logic such as submitting */
+{
+  let addTouchedClass = function () {
+    this.classList.add('contact-form__input-field--is-touched')
+  }
+  document
+    .querySelectorAll('input')
+    .forEach((element) => {
+      element.addEventListener('blur', addTouchedClass, false)
+      element.addEventListener('keydown', addTouchedClass, false)
+    })
+  document
+    .querySelectorAll('textarea')
+    .forEach((element) => {
+      element.addEventListener('blur', addTouchedClass, false)
+      element.addEventListener('keydown', addTouchedClass, false)
+    })
+}
+
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", handleFormSubmit);
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  postFormData(formData).then(() => {
+    document.getElementById('contact-form').reset();
+    document.getElementById('contact-form').style.display = "none";
+    document.getElementById('contact-form-success').style.display = "block";
+  });
+}
+
+function postFormData(formData) {
+
+  return fetch("handle_contact-form.php", {
+    method: "POST",
+    body: formData
+  });
+}
---
+++ style-guide.md
@@ -0,0 +1,60 @@
+# Front-end Style Guide
+
+## Layout
+
+### Breakpoints
+
+- Small: 600px
+- Medium: 800px
+
+## Colors
+
+### Primaries
+
+- Brand (Purple): rgb(105, 25, 255)
+- Brand v2 (Light Purple): rgb(151, 84, 239)
+- Pinkish Red: rgb(255, 70, 101)
+- Cyan: rgb(72, 227, 170)
+
+### Neutrals
+
+- Dark Not Black (Very Dark Gray): rgb(15, 25, 34)
+- Not Quite Black (Dark Gray): rgb(31, 39, 49)
+- Grayish Black (Gray): rgb(45, 53, 62)
+- Off White (White): rgb(255, 254, 250)
+
+### Gradients
+
+- Brand (Purple to Light Purple): linear-gradient(90deg, #6A19FF 0%, #9754EF 100%)
+
+### Opacities
+
+- Not Much Transparency: 0.85
+- Somewhat Translucent: 0.65
+- Almost Invisible: 0.35
+
+## Typography
+
+### Type Scale (Font Sizes)
+
+- Headline Gigantic (Headline 1): clamp(2.9rem, 1.34rem + 6.24vw, 6.8rem)
+- Headline (Headline 2): clamp(2.125rem, 1.3125rem + 3.25vw, 3.75rem)
+- Smaller Headline (Headline 3): clamp(1.35rem, 0.9625rem + 1.55vw, 2.125rem)
+- Body (Body 1): clamp(0.8rem, 0.7rem + 0.4vw, 1rem)
+- Smaller Body (Body 2): clamp(0.5rem, 0.375rem + 0.5vw, 0.75rem)
+
+*Font sizes use the clamp() method in order to smoothly scale with screen size*
+
+### Fonts
+
+- Family: [Roboto](https://fonts.google.com/specimen/Roboto)
+- Weights: 400 (Standard), 700 (Headings)
+- Usage: Base font / mostly body text
+
+- [Roboto Condensed](https://fonts.google.com/specimen/Roboto+Condensed)
+- Weights: 400 (Standard), 700 (Headings)
+- Usage: Headlines
+
+- Family: [Roboto Mono](https://fonts.google.com/specimen/Roboto+Mono)
+- Weights: 400 (Standard), 700 (Headings)
+- Usage: Typewriter animation
