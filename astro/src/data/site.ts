c13932703ef9e2e9ef145c20017cd99091f67e47
LOKKEE <kilian_peters@gmx.de>
2023-03-07 21:29:36 +0100

    Cleaned up site data

--- astro/src/data/site.ts
+++ astro/src/data/site.ts
@@ -1,14 +1,9 @@
 export default {
-  // website description
-  description: `I'm Lokkee, a passionate freelancer bringing you programming and design from the future. I am experienced in developing web and desktop applications including full front end design. This includes brand identity, graphics and illustrations.`,
-  // contact email
-  email: `hello@lokkeestudios.com`,
-  // site url
-  url: `https://lokkeestudios.com`,
-  // source code url
-  sourcecodeURL: `https://sourcecode.lokkeestudios.com`,
-  // github url
-  githubURL: `https://github.lokkeestudios.com`,
-  // discord server url
-  discordURL: `https://discord.lokkeestudios.com`,
+  description:
+    "I'm Lokkee, a passionate freelancer bringing you programming and design from the future. I am experienced in developing websites and web applications including full front end design. This includes brand identity, graphics and illustrations.",
+  email: 'hello@lokkeestudios.com',
+  siteUrl: 'https://lokkeestudios.com',
+  sourcecodeURL: 'https://sourcecode.lokkeestudios.com',
+  githubURL: 'https://github.lokkeestudios.com',
+  discordURL: 'https://discord.lokkeestudios.com',
 };
