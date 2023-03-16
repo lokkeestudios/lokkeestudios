1015a7143e0e8675b6e09082dd6f0dd98d284fb2
LOKKEE <kilian_peters@gmx.de>
2023-03-16 19:43:07 +0100

    Turned Google Analytics related ids into environment variables

--- astro/src/env.d.ts
+++ astro/src/env.d.ts
@@ -4,6 +4,8 @@ interface ImportMetaEnv {
   readonly PUBLIC_SANITY_PROJECT_ID: string;
   readonly PUBLIC_SANITY_DATASET: string;
   readonly PUBLIC_SANITY_API_VERSION: string;
+  readonly PUBLIC_GOOGLE_SITE_VERIFICATION: string;
+  readonly PUBLIC_GOOGLE_ANALYTICS_ID: string;
 }
 
 interface ImportMeta {
