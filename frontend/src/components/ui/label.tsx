2a843761f143c635e49a8154e39b8ba5b4de0228
LOKKEE <kilian_peters@gmx.de>
2023-05-20 00:09:51 +0200

    Changed label component from radix to plain old component

--- frontend/src/components/ui/label.tsx
+++ frontend/src/components/ui/label.tsx
@@ -1,20 +1,24 @@
 import { cn } from '@/lib/utils';
-import * as LabelPrimitive from '@radix-ui/react-label';
-import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react';
+import { LabelHTMLAttributes, ReactNode, forwardRef } from 'react';
 
-const Label = forwardRef<
-  ElementRef<typeof LabelPrimitive.Root>,
-  ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root
-    ref={ref}
-    className={cn(
-      'mb-2 block text-sm uppercase leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
-      className,
-    )}
-    {...props}
-  />
-));
+interface LabelProps extends LabelHTMLAttributes<HTMLLabelElement> {
+  children: ReactNode;
+}
+
+const Label = forwardRef<HTMLLabelElement, LabelProps>(
+  ({ children, className, ...props }, ref) => (
+    <label
+      ref={ref}
+      className={cn(
+        'mb-2 block text-sm uppercase leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </label>
+  ),
+);
 Label.displayName = 'Label';
 
 export default Label;
