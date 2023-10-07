95bdbf410e90a53de7690a97569ed7b860bd3061
LOKKEE <kilian_peters@gmx.de>
2023-10-07 16:56:38 +0800

    feat(frontend): add scroll progress indicator

---
+++ frontend/src/components/blog/scroll-progress.tsx
@@ -0,0 +1,17 @@
+import { motion, useScroll, useTransform } from 'framer-motion';
+
+function ScrollProgress() {
+  const { scrollYProgress } = useScroll();
+  const progressWidth = useTransform(scrollYProgress, [0, 1], ['0%', '100%']);
+
+  return (
+    <div className="fixed inset-x-0 top-0 z-[10000] h-px bg-neutrals-600">
+      <motion.div
+        style={{ width: progressWidth }}
+        className="h-full bg-gradient-to-r from-neutrals-100/30 via-neutrals-100 to-neutrals-100/30"
+      />
+    </div>
+  );
+}
+
+export default ScrollProgress;
