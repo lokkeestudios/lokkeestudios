9d05a8e4266b5261bf8a03ff7e1f06751e6325f5
LOKKEE <kilian_peters@gmx.de>
2023-10-20 17:34:39 +0800

    style: change inputs caret color

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -8,7 +8,7 @@ type TextareaProps = TextareaHTMLAttributes<TextareaHTMLElement>;
 const Textarea = forwardRef<TextareaHTMLElement, TextareaProps>(({ className, ...props }, ref) => (
   <textarea
     className={cn(
-      'peer block max-h-36 min-h-[3.125rem] w-full appearance-none rounded-sm border border-neutrals-600 bg-neutrals-800 p-3 shadow-[0_0_10px,_inset_0_0_10px] shadow-transparent transition-colors focus:border-primary focus:shadow-primary/70 focus:outline-none disabled:cursor-not-allowed disabled:opacity-70',
+      'peer block max-h-36 min-h-[3.125rem] w-full appearance-none rounded-sm border border-neutrals-600 bg-neutrals-800 p-3 caret-primary shadow-[0_0_10px,_inset_0_0_10px] shadow-transparent transition-colors focus:border-primary focus:shadow-primary/70 focus:outline-none disabled:cursor-not-allowed disabled:opacity-70',
       className,
     )}
     ref={ref}
