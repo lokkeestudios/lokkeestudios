ca1ba49087ec6b339fec16e5fdf4c71edf076847
LOKKEE <kilian_peters@gmx.de>
2024-09-28 11:31:28 +0800

    chore: update dependencies

--- src/lib/sanity/sanity-image.ts
+++ src/lib/sanity/sanity-image.ts
@@ -1,5 +1,5 @@
 import imageUrlBuilder from '@sanity/image-url';
-import { type SanityImageSource } from '@sanity/image-url/lib/types/types';
+import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
 import { sanityClient } from 'sanity:client';
 
 type ImageAssetMetadataPaletteColor = {
